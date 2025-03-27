36b13b1359ce06d2d7d224dd8c0817ff767fc5fc
ignSimix <gj.strujevics@gmail.com>
2025-03-27 11:05:26 +0200

    3 enemys

--- rpg/Assets/Scripts/Enemy.cs
+++ rpg/Assets/Scripts/Enemy.cs
@@ -4,5 +4,57 @@ using UnityEngine;
 
 public class Enemy : Character
 {
-    [SerializeField] internal int aggression = 10;
+    [Header("Enemy Rewards")]
+    [SerializeField] protected int expReward = 30;
+    [SerializeField] protected int goldReward = 15;
+    public int ExpReward => expReward;
+    public int GoldReward => goldReward;
+
+    protected override void Start()
+    {
+        base.Start();
+        nameText.text = CharacterName;
+    }
+
+    public override void SpecialAbility()
+    {
+        // Enemy-specific special ability
+        Debug.Log($"{CharacterName} uses special ability!");
+    }
+    public class Goblin : Enemy
+    {
+        private void Awake()
+        {
+            CharacterName = "Goblin";
+            maxHealth = 60;
+        }
+
+        public override int Attack()
+        {
+            int damage = base.Attack();
+
+            // 30% chance to attack twice
+            if (Random.value < 0.3f)
+            {
+                damage += base.Attack();
+                Debug.Log("Goblin attacks again!");
+            }
+
+            return damage;
+        }
+    }
+
+    public class Orc : Enemy
+    {
+        private void Awake()
+        {
+            CharacterName = "Orc";
+            maxHealth = 120;
+        }
+
+        public override int Attack()
+        {
+            return Mathf.RoundToInt(base.Attack() * 1.5f); // Orcs sit stiprak bet lenak
+        }
+    }
 }
